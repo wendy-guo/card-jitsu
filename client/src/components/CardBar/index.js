471438ae82b188b4796c285e5497010cb005a8e9
wendy-guo <wendy.guo@live.com>
2020-11-29 12:34:21 -0500

    added transitions for dealing/playing cards

--- client/src/components/CardBar/index.js
+++ client/src/components/CardBar/index.js
@@ -1,19 +1,26 @@
 import "./style.css";
 import Card from "../Card";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function CardBar(props) {
   return (
-    <div className={props.playable ? "card-bar playable" : "card-bar"}>
-      {props.cards.map((card, i) => (
-        <Card
-          index={i}
-          key={i}
-          card={card}
-          height="200px"
-          width="140px"
-          onCardClick={(index) => props.onCardClick(index)}
-        />
-      ))}
+    <div>
+      <TransitionGroup
+        className={props.playable ? "card-bar playable" : "card-bar"}
+      >
+        {props.cards.map((card, i) => (
+          <CSSTransition key={i} timeout={500} classNames="card">
+            <Card
+              index={i}
+              key={i}
+              card={card}
+              height="200px"
+              width="140px"
+              onCardClick={(index) => props.onCardClick(index)}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 }
