d0ec07dd1c249ccd7d3f58fb1a034bf452b4116f
wendy-guo <wendy.guo@live.com>
2020-11-29 22:06:40 -0500

    created title screen

--- client/src/components/MainScreen/index.js
+++ client/src/components/MainScreen/index.js
@@ -1,11 +1,37 @@
 import { NavLink } from "react-router-dom";
+import "./style.css";
+
+const howToPlay = () => {
+  console.log("here are the rules");
+};
 
 function MainScreen(props) {
   return (
-    <div>
-      <NavLink to="/match">
-        <button onClick={props.onStartClick}>start game</button>
-      </NavLink>
+    <div className="main-screen">
+      <div className="title">
+        <div>
+          <span style={{ transform: "rotate(-10deg) translateY(4px)" }}>C</span>
+          <span style={{ transform: "rotate(7deg) translateY(5px)" }}>a</span>
+          <span style={{ transform: "rotate(4deg) translateY(2px)" }}>r</span>
+          <span style={{ transform: "rotate(-3deg) translateY(1px)" }}>d</span>
+        </div>
+        <div>
+          <span style={{ transform: "rotate(-8deg) translateY(3px)" }}>J</span>
+          <span style={{ transform: "rotate(10deg) translateY(1px)" }}>i</span>
+          <span style={{ transform: "rotate(5deg) translateY(4px)" }}>t</span>
+          <span style={{ transform: "rotate(3deg) translateY(0px)" }}>s</span>
+          <span style={{ transform: "rotate(9deg) translateY(5px)" }}>u</span>
+        </div>
+      </div>
+      <button onClick={howToPlay()}>
+        <span className="button-text">how to play</span>
+      </button>
+
+      <button onClick={props.onStartClick} style={{ marginBottom: "100px" }}>
+        <NavLink to="/match">
+          <span className="button-text">start game</span>
+        </NavLink>
+      </button>
     </div>
   );
 }
