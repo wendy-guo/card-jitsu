ec6833382198435256d6e072a70a9aa9830f2685
wendy-guo <wendy.guo@live.com>
2020-12-14 16:01:51 -0500

    moved api call for dealed card to CardBar

--- client/src/components/CardBar/index.js
+++ client/src/components/CardBar/index.js
@@ -1,26 +1,64 @@
 import "./style.css";
 import Card from "../Card";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { useState } from "react";
+
+const getDealedCards = async (match_id) => {
+  const request = new Request(`/deal-cards?match_id=${match_id}`, {
+    method: "get",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+  });
+
+  try {
+    const res = await fetch(request);
+    if (res.status === 200) {
+      return res.json();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 function CardBar(props) {
+  const [cards, set] = useState(props.cards);
+
+  const dealCards = (match_id) => {
+    getDealedCards(match_id).then((card) => {
+      cards[cards.findIndex((card) => card === null)] = card;
+      set(cards);
+      console.log("cards have been dealt");
+      props.onDealCards();
+    });
+  };
+
+  const handleCardClick = (i) => {
+    if (props.playable) {
+      props.onCardClick(cards[i]);
+      cards[i] = null;
+      console.log(cards);
+      set(cards);
+    }
+  };
+
+  if (props.dealCards && cards.includes(null)) {
+    dealCards(props.match);
+  }
+
   return (
-    <div>
-      <TransitionGroup
-        className={props.playable ? "card-bar playable" : "card-bar"}
-      >
-        {props.cards.map((card, i) => (
-          <CSSTransition key={i} timeout={2000} classNames="card">
-            <Card
-              index={i}
-              key={i}
-              card={card}
-              height="200px"
-              width="140px"
-              onCardClick={(index) => props.onCardClick(index)}
-            />
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
+    <div className={props.playable ? "card-bar playable" : "card-bar"}>
+      {cards.map((card, i) => (
+        <Card
+          style={props}
+          key={i}
+          index={i}
+          card={card}
+          height="200px"
+          width="140px"
+          onCardClick={(i) => handleCardClick(i)}
+        />
+      ))}
     </div>
   );
 }
