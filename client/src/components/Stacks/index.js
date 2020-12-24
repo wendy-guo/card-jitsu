502ce5679fd190fbbbc814f29b742ef9de80353f
wendy-guo <wendy.guo@live.com>
2020-12-24 15:03:03 -0500

    made size of icons variable

--- client/src/components/Stacks/index.js
+++ client/src/components/Stacks/index.js
@@ -3,14 +3,14 @@ import { GiWaterDrop } from "react-icons/gi";
 import { AiFillFire } from "react-icons/ai";
 import { IoIosSnow } from "react-icons/io";
 
-const getIcon = (type) => {
+const getIcon = (type, size) => {
   switch (type) {
     case "snow":
-      return <IoIosSnow size="40px" fill="white" />;
+      return <IoIosSnow size={size} fill="white" />;
     case "water":
-      return <GiWaterDrop size="40px" fill="rgb(133, 193, 197)" />;
+      return <GiWaterDrop size={size} fill="rgb(133, 193, 197)" />;
     case "fire":
-      return <AiFillFire size="40px" fill="rgb(172, 32, 32)" />;
+      return <AiFillFire size={size} fill="rgb(172, 32, 32)" />;
     default:
       return;
   }
@@ -24,21 +24,22 @@ function StackCard(props) {
       style={{
         backgroundColor: "#".concat(props.colour),
         bottom: (props.offset * 30).toString().concat("px"),
+        zIndex: 99 - props.offset,
       }}
     >
-      {getIcon(props.type)}
+      {getIcon(props.type, props.size)}
     </div>
   );
 }
 
 function Stacks(props) {
-  console.log(props.newest);
   return (
     <div className="stacks">
       <div className="stack">
         {props.stacks["snow"].map((colour, i) => (
           <StackCard
             type="snow"
+            size="50px"
             colour={colour}
             offset={i}
             last={
@@ -51,6 +52,7 @@ function Stacks(props) {
         {props.stacks["water"].map((colour, i) => (
           <StackCard
             type="water"
+            size="50px"
             colour={colour}
             offset={i}
             last={
@@ -63,6 +65,7 @@ function Stacks(props) {
         {props.stacks["fire"].map((colour, i) => (
           <StackCard
             type="fire"
+            size="50px"
             colour={colour}
             offset={i}
             last={
@@ -76,3 +79,4 @@ function Stacks(props) {
 }
 
 export default Stacks;
+export { StackCard };
