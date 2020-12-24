ac4d72989abc53ff343cc1a2ee2aef9ecad73887
wendy-guo <wendy.guo@live.com>
2020-12-23 20:54:21 -0500

    added animation and icons

--- client/src/components/Stacks/index.js
+++ client/src/components/Stacks/index.js
@@ -1,48 +1,86 @@
 import "./style.css";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { GiWaterDrop } from "react-icons/gi";
+import { AiFillFire } from "react-icons/ai";
+import { IoIosSnow } from "react-icons/io";
 
 function SnowCard(props) {
+  console.log(props.last);
   return (
-    <CSSTransition timeout={500} className="snow-card">
-      <div className="stack-card" style={{ backgroundColor: props.colour }}>
-        snow
-      </div>
-    </CSSTransition>
+    <div
+      className={props.last ? "stack-card last" : "stack-card"}
+      style={{
+        backgroundColor: "#".concat(props.colour),
+        bottom: (props.offset * 25).toString().concat("px"),
+      }}
+    >
+      <IoIosSnow size="40px" fill="white" />
+    </div>
   );
 }
 
 function WaterCard(props) {
   return (
-    <div className="stack-card" style={{ backgroundColor: props.colour }}>
-      water
+    <div
+      className={props.last ? "stack-card last" : "stack-card"}
+      style={{
+        backgroundColor: "#".concat(props.colour),
+        bottom: (props.offset * 25).toString().concat("px"),
+      }}
+    >
+      <GiWaterDrop size="40px" fill="rgb(133, 193, 197)" />
     </div>
   );
 }
 
 function FireCard(props) {
   return (
-    <div className="stack-card" style={{ backgroundColor: props.colour }}>
-      fire
+    <div
+      className={props.last ? "stack-card last" : "stack-card"}
+      style={{
+        backgroundColor: "#".concat(props.colour),
+        bottom: (props.offset * 25).toString().concat("px"),
+      }}
+    >
+      <AiFillFire size="40px" fill="rgb(172, 32, 32)" />
     </div>
   );
 }
 
 function Stacks(props) {
+  console.log(props.newest);
   return (
     <div className="stacks">
       <div className="stack">
-        {props.stacks["snow"].map((colour) => (
-          <SnowCard colour={colour} />
+        {props.stacks["snow"].map((colour, i) => (
+          <SnowCard
+            colour={colour}
+            offset={i}
+            last={
+              i === props.stacks["snow"].length - 1 && props.newest === "snow"
+            }
+          />
         ))}
       </div>
       <div className="stack">
-        {props.stacks["water"].map((colour) => (
-          <WaterCard colour={colour} />
+        {props.stacks["water"].map((colour, i) => (
+          <WaterCard
+            colour={colour}
+            offset={i}
+            last={
+              i === props.stacks["water"].length - 1 && props.newest === "water"
+            }
+          />
         ))}
       </div>
       <div className="stack">
-        {props.stacks["fire"].map((colour) => (
-          <FireCard colour={colour} />
+        {props.stacks["fire"].map((colour, i) => (
+          <FireCard
+            colour={colour}
+            offset={i}
+            last={
+              i === props.stacks["fire"].length - 1 && props.newest === "fire"
+            }
+          />
         ))}
       </div>
     </div>
