bb8c3ad9e66bb9f89012c7a2f13460e2a6c09157
wendy-guo <wendy.guo@live.com>
2020-12-25 18:42:30 -0500

    animated opponent card flip

--- client/src/components/PlayedCards/index.js
+++ client/src/components/PlayedCards/index.js
@@ -12,17 +12,27 @@ function PlayedCards(props) {
         margin: "0px auto 60px auto",
       }}
     >
-      {props.opponentCard ? (
-        <Card
-          index={-1}
-          card={props.opponentCard}
-          height="300px"
-          width="210px"
-          onCardClick={(_) => null}
-        />
-      ) : (
-        <div className="placeholder-card"> waiting for opponent </div>
-      )}
+      {
+        // if props.reveal, flip the Card component below
+        props.opponentCard ? (
+          <div className="opponent-card">
+            <div className={props.reveal ? "inner-card reveal" : "inner-card"}>
+              <div className="card-back"></div>
+              <div className="card-front">
+                <Card
+                  index={-1}
+                  card={props.opponentCard}
+                  height="300px"
+                  width="210px"
+                  onCardClick={(_) => null}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="placeholder-card"> waiting for opponent </div>
+        )
+      }
       {props.playerCard ? (
         <Card
           index={-1}
