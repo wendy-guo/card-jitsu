71ab564309713ba16edf1cce6389498411b422a8
wendy-guo <wendy.guo@live.com>
2020-12-25 18:41:49 -0500

    added icons and images to card faces

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,4 +1,80 @@
 import "./style.css";
+import { GiWaterDrop } from "react-icons/gi";
+import { AiFillFire } from "react-icons/ai";
+import { IoIosSnow } from "react-icons/io";
+import ruby from "../../villagers/ruby.png";
+import marina from "../../villagers/marina.png";
+import apple from "../../villagers/apple.png";
+import aurora from "../../villagers/aurora.png";
+
+const getIcon = (type, size) => {
+  console.log(type);
+  switch (type) {
+    case "snow":
+      return <IoIosSnow size={size} fill="white" />;
+    case "water":
+      return <GiWaterDrop size={size} fill="rgb(133, 193, 197)" />;
+    case "fire":
+      return <AiFillFire size={size} fill="rgb(172, 32, 32)" />;
+    default:
+      return;
+  }
+};
+
+const getVillager = (colour, height, width) => {
+  switch (colour) {
+    case "d95252": // red
+      return (
+        <img
+          class="villager-image"
+          height={height}
+          width={width}
+          src={apple}
+          alt="apple"
+        />
+      );
+    case "ffd4d4": // pink
+      return (
+        <img
+          class="villager-image"
+          height={height}
+          width={width}
+          src={ruby}
+          alt="ruby"
+        />
+      );
+    case "9987cc": // purple
+      return (
+        <img
+          class="villager-image"
+          height={height}
+          width={width}
+          src={marina}
+          alt="marina"
+        />
+      );
+    case "a1e9ff": //blue
+      return (
+        <img
+          class="villager-image"
+          height={height}
+          width={width}
+          src={aurora}
+          alt="aurora"
+        />
+      );
+    default:
+      return (
+        <img
+          class="villager-image"
+          height={height}
+          width={width}
+          src={ruby}
+          alt="ruby"
+        />
+      );
+  }
+};
 
 function Card(props) {
   return props.card ? (
@@ -24,15 +100,20 @@ function Card(props) {
         style={{
           backgroundColor: "#".concat(props.card.colour),
           borderBottomRightRadius: "10px",
-          width: "30px",
-          height: "40px",
+          width: "45px",
+          height: "80px",
           fontSize: "30px",
           fontWeight: 600,
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        {props.card.number}
+        <div style={{ height: "30px" }}>{getIcon(props.card.type, "35px")}</div>
+        <div style={props.card.number > 9 ? {} : { marginLeft: "20%" }}>
+          {props.card.number}
+        </div>
       </div>
-      type {props.card.type}
+      {getVillager(props.card.colour, props.height, props.width)}
     </div>
   ) : (
     <div
