2c157b41cfcd33d56c124e5bbc08350c2c88a0b4
wendy-guo <wendy.guo@live.com>
2020-12-25 19:49:02 -0500

    added you win/lost under winning stack

--- client/src/components/WinStack/index.js
+++ client/src/components/WinStack/index.js
@@ -5,15 +5,28 @@ function WinStack(props) {
   console.log(props);
   return (
     <div className="win-stack">
-      {props.cards.map((card) => (
-        <StackCard
-          type={card.type}
-          size="70px"
-          colour={card.colour}
-          offset={0}
-          last={true}
-        />
-      ))}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {props.cards.map((card) => (
+          <StackCard
+            type={card.type}
+            size="70px"
+            colour={card.colour}
+            offset={0}
+            last={true}
+          />
+        ))}
+      </div>
+      {props.winner === "player" ? (
+        <div className="result">you win!!!</div>
+      ) : (
+        <div className="result">you lost :(</div>
+      )}
     </div>
   );
 }
