af37bfc6f12836a96c3121f92d0ede3ff3e1363a
wendy-guo <wendy.guo@live.com>
2020-12-27 13:07:42 -0500

    changed mongoose connection

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://wendy:foJX6oTOtREPfekG@cluster0.cgpjz.mongodb.net/test?authSource=admin&replicaSet=atlas-12ab6p-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" ||
+  "mongodb+srv://wendy:foJX6oTOtREPfekG@cluster0.pdj9z.mongodb.net/card-jitsu?retryWrites=true&w=majority" ||
     process.env.MONGODB_URI ||
     "mongodb://localhost:27017/Card-Jitsu",
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
