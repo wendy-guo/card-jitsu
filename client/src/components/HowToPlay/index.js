c893ce6828782fa1796f2f14dfc5c7340ef5cd4f
wendy-guo <wendy.guo@live.com>
2020-12-29 15:31:32 -0500

    finished added instructions

--- client/src/components/HowToPlay/index.js
+++ client/src/components/HowToPlay/index.js
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
 import "./style.css";
 import background from "../../assets/background.png";
-import page4 from "../../assets/page4.png";
-import apple from "../../villagers/apple.png";
+import page1 from "../../assets/page1.png";
+import page2 from "../../assets/page3.png";
+import page3 from "../../assets/page4.png";
 import Carousel from "react-bootstrap/Carousel";
 
 function Page(props) {
@@ -49,17 +50,28 @@ function HowToPlay(_) {
       <Carousel>
         <Carousel.Item>
           <Page
-            image={page4}
-            text="The objective of the game is to collect three cards of different colours; either of one element, or one of each element. "
+            image={page1}
+            text="Each card has three attributes: value (ranges from 2-12), element (water, snow, or fire), and colour (pink, red, blue, purple, orange, or green)."
           />
         </Carousel.Item>
         <Carousel.Item>
           <Page
-            image={page4}
-            text="Each card has three attributes: value (ranges from 2-12), element (water, snow, or fire), and colour (pink, red, blue, purple, orange, or green)."
+            image={page2}
+            text="Each element defeats another. Water puts out fire, fire melts snow, while snow freezes water. If the elements are the same, the higher number wins, otherwise it's a tie."
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Page
+            image={page3}
+            text="The objective of the game is to collect three cards of different colours; either of one element, or one of each element. "
           />
         </Carousel.Item>
       </Carousel>
+        <button style={{ marginTop: "10%" }}>
+          <NavLink to="/">
+            <span className="button-text">back</span>
+          </NavLink>
+        </button>
     </div>
   );
 }
